3c6f8138bc186c09aa93aa659f35db5f7d32a6a1
carlosrivera22 <carlosgrivera@Carloss-MacBook-Pro-82.local>
2016-09-04 17:10:26 -0400

    ip
    ip

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ jQuery(function($) {
 	//Initiat WOW JS
 	new WOW().init();
 
-    // one page navigation 
+    // one page navigation
     $('.main-navigation').onePageNav({
             currentClass: 'active'
     });
@@ -27,55 +27,6 @@ jQuery(function($) {
 	});
 
 
-/**
- * main.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2014, Codrops
- * http://www.codrops.com
- */
-(function() {
 
-	var bodyEl = document.body,
-		content = document.querySelector( '.contents' ),
-		openbtn = document.getElementById( 'open-button' ),
-		closebtn = document.getElementById( 'close-button' ),
-		isOpen = false;
-
-	function init() {
-		initEvents();
-	}
-
-	function initEvents() {
-		openbtn.addEventListener( 'click', toggleMenu );
-		if( closebtn ) {
-			closebtn.addEventListener( 'click', toggleMenu );
-		}
-
-		// close the menu element if the target it´s not the menu element or one of its descendants..
-		content.addEventListener( 'click', function(ev) {
-			var target = ev.target;
-			if( isOpen && target !== openbtn ) {
-				toggleMenu();
-			}
-		} );
-	}
-
-	function toggleMenu() {
-		if( isOpen ) {
-			classie.remove( bodyEl, 'show-menu' );
-		}
-		else {
-			classie.add( bodyEl, 'show-menu' );
-		}
-		isOpen = !isOpen;
-	}
-
-	init();
-
-})();
 
 });
